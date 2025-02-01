1c1362ffdf83406680d8462bf2cbe413b972494a
LabibMuhd <sleekbiz101@gmail.com>
2025-02-01 17:31:34 +0100

    initial commit

---
+++ js/admin.js
@@ -0,0 +1,143 @@
+const listHover = document.querySelectorAll(".admin-nav li");
+const dashBtn = document.querySelector(".dash");
+const dashboard = document.querySelector(".dashboard");
+const users = document.querySelector(".users-section");
+const userbtn = document.querySelector(".user");
+const shipment = document.querySelector(".shipment-section");
+const shipmentBtn = document.querySelector(".shipments");
+const shipmentForm = document.querySelector("#shipment-form");
+const addShipmentForm = document.querySelector(".add-ship");
+const addShipmentIcon = document.querySelector(".add-ship-icon");
+const closeShipmentIcon = document.querySelector(".close-ship-icon");
+const editShipmentForm = document.querySelectorAll(".edit");
+const deleteShipmentForm = document.querySelectorAll(".delete");
+const settings = document.querySelector(".settings-section");
+const cardHeader = document.querySelector(".head");
+const shipTable = document.querySelector(".ship-table");
+const settingsBtn = document.querySelector(".setting");
+const settingsForm = document.querySelector("#settings-form");
+const editBtn = document.querySelector(".edit-btn");
+const exitBtn = document.querySelector(".exit-btn");
+const settingsBox = document.querySelector(".settings-box");
+const popupDiv = document.querySelector(".popup-div");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+const toggleForm = function () {
+  shipmentForm.classList.toggle("hidden");
+};
+const toggleTable = function () {
+  shipTable.classList.toggle("hidden");
+};
+const toggleDashboard = function () {
+  dashboard.classList.toggle("hidden");
+};
+const toggleUsers = function () {
+  users.classList.toggle("hidden");
+};
+const toggleShipment = function () {
+  shipment.classList.toggle("hidden");
+};
+const toggleAdd = function () {
+  addShipmentIcon.classList.toggle("hidden");
+};
+const toggleClose = function () {
+  closeShipmentIcon.classList.toggle("hidden");
+};
+
+const activeLink = function () {
+  listHover.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+};
+
+listHover.forEach((item) => item.addEventListener("click", activeLink));
+
+// menu toggle
+const toggle = document.querySelector(".menu-box");
+const navigation = document.querySelector(".admin-nav");
+const main = document.querySelector(".main");
+
+const collapseNav = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
+
+toggle.addEventListener("click", collapseNav);
+
+// dashboard toggle
+dashBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  dashboard.classList.remove("hidden");
+  users.classList.add("hidden");
+  shipment.classList.add("hidden");
+  settings.classList.add("hidden");
+});
+// nav toggle
+userbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  dashboard.classList.add("hidden");
+  users.classList.remove("hidden");
+  shipment.classList.add("hidden");
+  settings.classList.add("hidden");
+});
+// shipment toggle
+shipmentBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  dashboard.classList.add("hidden");
+  shipment.classList.remove("hidden");
+  users.classList.add("hidden");
+  settings.classList.add("hidden");
+});
+// add shipment
+const toggleShip = function (e) {
+  e.preventDefault();
+  if (shipmentForm.classList.contains("hidden")) {
+    toggleAdd();
+    toggleTable();
+    toggleForm();
+    toggleClose();
+  } else {
+    toggleTable();
+    toggleForm();
+    toggleClose();
+    toggleAdd();
+  }
+};
+
+addShipmentForm.addEventListener("click", toggleShip);
+
+editShipmentForm.forEach((item) => {
+  item.addEventListener("click", toggleShip);
+});
+
+deleteShipmentForm.forEach((item) => {
+  item.addEventListener("click", function () {
+    popupDiv.classList.toggle("hidden");
+  });
+});
+
+settingsBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  dashboard.classList.add("hidden");
+  shipment.classList.add("hidden");
+  users.classList.add("hidden");
+  settings.classList.remove("hidden");
+  settingsForm.classList.add("hidden");
+  settingsBox.classList.remove("hidden");
+});
+
+editBtn.addEventListener("click", function (e) {
+  settingsForm.classList.remove("hidden");
+  settingsBox.classList.add("hidden");
+});
+
+exitBtn.addEventListener("click", function (e) {
+  settingsForm.classList.add("hidden");
+  settingsBox.classList.remove("hidden");
+});
+
+noBtn.addEventListener("click", function (e) {
+  popupDiv.classList.toggle("hidden");
+});
---
+++ js/script.js
@@ -0,0 +1,160 @@
+let yearEL = document.querySelector(".year");
+const btnScrollTo = document.querySelector(".btn--start");
+const signUpSection = document.querySelector("#signup");
+const allSections = document.querySelectorAll(".section");
+const form = document.querySelector("#login");
+const loginForm = document.querySelector("#popup");
+const orderForm = document.querySelector("#pform");
+const formContainer = document.querySelector("#popup-form");
+const signUp = document.querySelector("#sign_up");
+const contactUs = document.querySelector(".nav-cta");
+const login = document.querySelector(".nav-login");
+const close = document.querySelector(".popup__close");
+const pass = document.querySelector("#pass");
+const forgotPass = document.querySelector(".sign-link");
+const fPass = document.querySelector("#fpass");
+const btnOrder = document.querySelectorAll(".btn--order");
+const popupClose = document.querySelectorAll(".popup__close");
+// SET CURRENT YEAR
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
+
+// REVEAL SECTIONS
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// SIGN UP
+signUp.addEventListener("submit", function (e) {
+  e.preventDefault();
+  loginForm.classList.remove("hidden");
+  window.open("#popup", "_self");
+});
+
+// LOGIN
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  contactUs.classList.remove("hidden");
+  login.classList.add("hidden");
+  loginForm.classList.add("hidden");
+
+  btnScrollTo.addEventListener("click", function (e) {
+    window.open("#popup-form", "_self");
+  });
+});
+forgotPass.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  window.open("#forgotpass", "_self");
+});
+fPass.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let html = `<div class="success-box u-margin-top-medium">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+    </svg>
+
+    <h2 class="heading-secondary u-margin-bottom-medium">Password reset link sent!</h2>
+    <p class='hero-description'>Check your email for a link to reset your password</p>
+  </div>`;
+  // orderForm.replaceWith(html);
+  fPass.insertAdjacentHTML("beforebegin", html);
+  fPass.classList.toggle("hidden");
+});
+// SMOOTH SCROLLING
+btnScrollTo.addEventListener("click", function (e) {
+  signUpSection.scrollIntoView({ behavior: "smooth" });
+});
+
+// ORDER FORM
+orderForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // formContainer.innerHTML = "";
+
+  let html = `<div class="success-box u-margin-top-medium">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+    </svg>
+
+    <h2 class="heading-secondary u-margin-bottom-medium">Response recieved</h2>
+  </div>`;
+  // orderForm.replaceWith(html);
+  orderForm.insertAdjacentHTML("beforebegin", html);
+  orderForm.classList.toggle("hidden");
+});
+
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll(".item");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${105 * (i - slide)}%)`)
+    );
+  };
+  const init = function () {
+    goToSlide(0);
+  };
+
+  init();
+  // move to ;next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+  };
+  // move to prev slide
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+  };
+
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+};
+slider();
+// ORDER button
+btnOrder.forEach(function (order) {
+  order.addEventListener("click", function (e) {
+    if (!login.classList.contains("hidden")) {
+      signUpSection.scrollIntoView({ behavior: "smooth" });
+    } else {
+      e.preventDefault();
+      window.open("#popup-form", "self");
+    }
+  });
+});
